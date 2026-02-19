892848166f3681192a5b6b0a118e43ddafc04381
Nitish <nitishkthakur012@gmail.com>
2026-02-19 23:40:48 +0530

    add admin router

---
+++ routes/admin.js
@@ -0,0 +1,39 @@
+const {Router} = require("express");
+const adminRouter = Router();
+
+// adminRouter.use(adminMiddleware)
+
+    adminRouter.post("/signup", function(req, res){
+        res.json({
+            message: "signup endpoint"
+        })
+    })
+    
+    
+    adminRouter.post("/signin", function(req, res){
+        res.json({
+            message: "signin endpoint"
+        })
+    })
+
+    adminRouter.post("/course", function(req, res){
+        res.json({
+            message: "signin endpoint"
+        })
+    })
+
+    adminRouter.post("/course", function(req, res){
+        res.json({
+            message: "signin endpoint"
+        })
+    })
+
+    adminRouter.post("/course/bulk", function(req, res){
+        res.json({
+            message: "signin endpoint"
+        })
+    })
+   
+    module.exports = {
+        adminRouter : adminRouter
+    }
